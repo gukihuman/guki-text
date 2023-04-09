f7043859e462dc8bd692525c7094d7c6aa5aa714
ivan-f-sokolov <sokolovivanf@gmail.com>
2023-04-09 22:45:47 +0500

    Add toString method

--- ts/importer.ts
+++ ts/importer.ts
@@ -37,6 +37,25 @@ class myLodash {
       }
     }
   }
+  public toString(value) {
+    if (typeof value === "string") {
+      return value
+    } else if (typeof value === "number" || typeof value === "boolean") {
+      return value.toString()
+    } else if (value === null) {
+      return "null"
+    } else if (value === undefined) {
+      return "undefined"
+    } else if (typeof value === "function") {
+      return value.toString()
+    } else if (Array.isArray(value)) {
+      return `[${value.map(myToString).join(",")}]`
+    } else if (typeof value === "object") {
+      return JSON.stringify(value)
+    } else {
+      return value.toString()
+    }
+  }
 }
 
 export const _ = new myLodash()
