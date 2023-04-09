b1d59a193273fd02e368c1412cab6bc8ac714758
ivan-f-sokolov <sokolovivanf@gmail.com>
2023-04-09 22:19:49 +0500

    Implement manual lodash

--- ts/importer.ts
+++ ts/importer.ts
@@ -1,3 +1,42 @@
-import * as lodash from "lodash"
+// import * as lodash from "lodash"
 
-export const _ = lodash
+// _.debounce implementation
+class myLodash {
+  public debounce(func, wait, immediate = false) {
+    let timeout
+
+    return function executedFunction() {
+      const context = this
+      const args = arguments
+
+      const later = function () {
+        timeout = null
+        if (!immediate) func.apply(context, args)
+      }
+
+      const callNow = immediate && !timeout
+      clearTimeout(timeout)
+
+      timeout = setTimeout(later, wait)
+
+      if (callNow) func.apply(context, args)
+    }
+  }
+
+  // _.forEach implementation
+  public forEach(collection, iteratee) {
+    if (Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        iteratee(collection[i], i)
+      }
+    } else if (typeof collection === "object") {
+      for (let key in collection) {
+        if (collection.hasOwnProperty(key)) {
+          iteratee(collection[key], key)
+        }
+      }
+    }
+  }
+}
+
+export const _ = new myLodash()
