6e71302f5025e9486081702ae9edcb334e8fda58
ivan-f-sokolov <sokolovivanf@gmail.com>
2023-04-16 22:15:46 +0500

    Add watcher por programmatically changed input value

--- components/input-area.vue
+++ components/input-area.vue
@@ -2,17 +2,21 @@
 div(class="flex justify-center w-full bg-gradient-to-b from-[#263242] to-gray-800 relative")
   div(class="relative p-2 bg-gray-700 flex justify-center w-full shadow-2xl max-w-[800px]")
     textarea(
+      ref="textarea"
       class="px-10 pt-8 w-full h-full outline-none max-w-[800px] bg-gray-700 p-4 text-lg text-gray-100 resize-none scrollbar relative"
-      v-model.trim="activeItem.text"
+      v-model="activeItem.text"
       :readonly="readonly"
       @input="states.saveToLocalStorageDebounced"
     )
 </template>
 
 <script setup lang="ts">
+const textarea: any = ref(null)
+
 const activeItem = computed(() => {
   return states.items.find((item) => item.id === states.activeId) || ""
 })
+
 const readonly = computed(() => {
   let isReadonly = true
   if (states.activeId) isReadonly = false
@@ -27,6 +31,15 @@ const readonly = computed(() => {
     isReadonly = true
   }
   return isReadonly
+})
+onMounted(() => {
+  // watch programmatically changed input value like Voice In plugin
+  setInterval(() => {
+    if (textarea.value.value !== activeItem.value.text) {
+      activeItem.value.text = textarea.value.value
+      states.saveToLocalStorageDebounced()
+    }
+  }, 150)
 })
 </script>
 <style>
